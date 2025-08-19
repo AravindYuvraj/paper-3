d0e016a5401b7e1b36ea35016f0f68eb0c6a1713
Aravind <aravind98761234@gmail.com>
2025-08-19 18:20:00 +0530

    add main.py

--- main.py
+++ main.py
@@ -25,3 +25,54 @@ all_splits = text_splitter.split_documents(data)
 print("************************************************")
 print(f"Split data into {len(all_splits)} sub-documents.")
 
+
+document_ids = vector_store.add_documents(documents=all_splits)
+
+print(document_ids[:3])
+
+from langchain import hub
+
+prompt = hub.pull("rlm/rag-prompt")
+
+example_messages = prompt.invoke(
+    {"context": "(context goes here)", "question": "(question goes here)"}
+).to_messages()
+
+assert len(example_messages) == 1
+print(example_messages[0].content)
+
+
+from langchain_core.documents import Document
+from typing_extensions import List, TypedDict
+
+
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+
+from langgraph.graph import START, StateGraph
+
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+
+
+result = graph.invoke({"question": "what is the nutrition density of  cream cheese?"})
+
+print(f"Context: {result['context']}\n\n")
+print(f"Answer: {result['answer']}")
